f0992e37dcc8a9700e45fd441d5db1996def0c62
all4x <estudar0101@gmail.com>
2021-10-12 13:22:39 -0300

    parei por limitação da lib requests

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 from time import sleep
 import requests as req
 from bs4 import BeautifulSoup
+
 from cookie import COOKIES
 
 url = 'https://www.instagram.com/explore/tags/alanzoka/'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+
 
 headers = {
     'User-Agent': (USER_AGENT),
@@ -15,6 +17,9 @@ r = req.get(url, headers = headers, cookies=(COOKIES))
 html_doc = r.text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
-for link in soup.find_all('link'):
-    print(link.get('href'))
 
+print(soup.prettify())
+
+
+# this script does not work because instagram page is generated with javascript
+# use selenium lib or playwrith :)
